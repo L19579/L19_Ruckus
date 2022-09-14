bcf3ea10b8769d4bef447c4fc4185137b1ea9564
JoJo <l19priv@gmail.com>
2022-09-14 17:21:46 -0400

    plugin, client cont'd

--- geyser/src/client.rs
+++ geyser/src/client.rs
@@ -1,14 +1,13 @@
 use {
     redis,
-    anyhow::{
-        anyhow, 
-        Result,
+    anyhow::Result,
+    crate::config::RedisConfig, 
+    solana_sdk::pubkey::Pubkey,
+    solana_geyser_plugin_interface::
+    geyser_plugin_interface::{
+        SlotStatus,
+        ReplicaAccountInfo,
     },
-    std::{
-        fs,
-        io::prelude::*,
-    },
-    crate::config::RedisConfig,
 };
 
 #[derive(Clone, Debug)]
@@ -17,13 +16,36 @@ pub struct RedisClient{
 }
 
 impl RedisClient{
-    pub fn new(redis_config: RedisConfig) -> Result<Self>{
-
+    pub fn new(redis_config: &RedisConfig) -> Result<Self>{
         let redis = redis::Client::
             open(redis_config.uri().as_str())
             .expect("Redis DB connection failed.");
-        return Ok(Self {
+        return Ok(Self{
             redis, 
         });
+    }
+
+    pub fn account_event(&mut self, slot: u64, account: &ReplicaAccountInfo) -> Result<()>{
+        let key = format!("account.{}", Pubkey::new(account.pubkey));
+        redis::cmd("HSET").arg(&key)
+            .arg("lamports").arg(account.lamports)
+            .arg("owner").arg(Pubkey::new(account.owner).to_string())
+            .arg("executable").arg(account.executable)
+            .arg("rent_epoch").arg(account.rent_epoch)
+            .arg("data").arg(account.data)
+            .arg("write_version").arg(account.write_version)
+            .arg("slot").arg(slot)
+            .execute(&mut self.redis.get_connection()?);
+        return Ok(());
+    }
+
+    pub fn slot_update_event(&mut self, slot: u64, parent: Option::<u64>, status: SlotStatus) 
+    -> Result<()>{
+        let key = format!("slot.{}", slot);
+        redis::cmd("HSET").arg(&key)
+            .arg("parent").arg(parent.unwrap_or(0)) // zero == none
+            .arg("status").arg(status.as_str())
+            .execute(&mut self.redis.get_connection()?);
+        return Ok(());
     }
 }
--- geyser/src/plugin.rs
+++ geyser/src/plugin.rs
@@ -1,4 +1,5 @@
-pub use {
+// unwraps ok.
+use {
     crate::{
         config::Config,
         client::RedisClient,
@@ -8,7 +9,6 @@ pub use {
         GeyserPlugin,
         SlotStatus,
         Result as GeyserResult,
-        ReplicaAccountInfo,
         ReplicaAccountInfoVersions,
         ReplicaTransactionInfoVersions,
         ReplicaBlockInfoVersions,
@@ -26,50 +26,60 @@ impl GeyserPlugin for GeyserRedisPlugin{
     }
 
     fn on_load(&mut self, _config_file: &str) -> GeyserResult<()>{
-        let redis_config = Config::load().unwrap().redis.unwrap(); // -- unwraps
-        self.redis_client = RedisClient::new(redis_config);
+        let redis_config = Config::load().unwrap().redis.unwrap();
+        self.redis_client = Some(RedisClient::new(&redis_config).unwrap());
         return Ok(());
     }
-
-    fn on_unload(&mut self){}
 
     fn update_account(
         &mut self,
         account: ReplicaAccountInfoVersions,
         slot: u64,
-        is_startup: bool,
+        _is_startup: bool,
     ) -> GeyserResult<()>{
-        // --
-        
+        let account = match account{
+            ReplicaAccountInfoVersions::V0_0_1(a) => a,
+        };
+
+        self.redis_client
+            .as_mut()
+            .unwrap()
+            .account_event(slot, &account)
+            .unwrap();
+
         return Ok(());
     }
-
-    fn notify_end_of_startup(&mut self) -> GeyserResult<()>{
-        return Ok(());
-    }
-
+    
     fn update_slot_status(
         &mut self,
         slot: u64,
         parent: Option<u64>,
         status: SlotStatus,
     ) -> GeyserResult<()>{
-        // -- 
+        self.redis_client
+            .as_mut()
+            .unwrap()
+            .slot_update_event(slot, parent, status)
+            .unwrap();
+
         return Ok(());
     }
-
+    
+    // Rest required for geyser interface but not used in this program.
+    fn on_unload(&mut self){}
+    fn notify_end_of_startup(&mut self) -> GeyserResult<()>{
+        return Ok(());
+    }
     fn notify_transaction(
         &mut self,
-        transaction: ReplicaTransactionInfoVersions,
-        slot: u64,
+        _transaction: ReplicaTransactionInfoVersions,
+        _slot: u64,
     ) -> GeyserResult<()>{
         return Ok(());
     }
-
-    /// ignored, non critical
     fn notify_block_metadata(
         &mut self,
-        blockinfo: ReplicaBlockInfoVersions,
+        _blockinfo: ReplicaBlockInfoVersions,
     ) -> GeyserResult<()>{
         return Ok(());
     }
